57370d46ba570ad753a8f573121fa944167a723a
Bau <baudelio_andalon@hotmail.com>
2024-02-01 19:47:35 -0600

    [FEATURE] New fonts

--- composeApp/src/commonMain/kotlin/com/borealnetwork/allen/components/Texts.kt
+++ composeApp/src/commonMain/kotlin/com/borealnetwork/allen/components/Texts.kt
@@ -1,17 +1,15 @@
 package com.borealnetwork.allen.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
-import com.borealnetwork.allen.theme.robotoBold
+import com.borealnetwork.allen.theme.boldTypo
 
 @Composable
 fun BoldText(
@@ -30,10 +28,9 @@ fun BoldText(
         fontSize = size,
         color = color,
         letterSpacing = 0.sp,
-        fontWeight = Bold,
         textAlign = align,
         overflow = textOverflow,
         maxLines = maxLines,
-        fontFamily = robotoBold()
+        fontFamily = boldTypo()
     )
 }
--- composeApp/src/commonMain/kotlin/com/borealnetwork/allen/theme/Theme.kt
+++ composeApp/src/commonMain/kotlin/com/borealnetwork/allen/theme/Theme.kt
@@ -9,8 +9,6 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Typography
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
@@ -19,77 +17,49 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.font.ResourceFont
+import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.font.FontWeight.Companion.Light
+import androidx.compose.ui.text.font.FontWeight.Companion.Medium
+import androidx.compose.ui.text.font.FontWeight.Companion.Normal
+import androidx.compose.ui.text.font.FontWeight.Companion.SemiBold
+import androidx.compose.ui.text.font.FontWeight.Companion.Thin
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.borealnetwork.allen.font
 
+private enum class TypographyEmbedded() {
+    ROBOTO, MONTSERRAT
+}
+
+private val DEFAULT_TYPOGRAPHY = TypographyEmbedded.ROBOTO
+
 private val LightColorScheme = lightColors(
     primary = md_theme_light_primary,
     onPrimary = md_theme_light_onPrimary,
-//    primaryContainer = md_theme_light_primaryContainer,
-//    onPrimaryContainer = md_theme_light_onPrimaryContainer,
     secondary = md_theme_light_secondary,
     onSecondary = md_theme_light_onSecondary,
-//    secondaryContainer = md_theme_light_secondaryContainer,
-//    onSecondaryContainer = md_theme_light_onSecondaryContainer,
-//    tertiary = md_theme_light_tertiary,
-//    onTertiary = md_theme_light_onTertiary,
-//    tertiaryContainer = md_theme_light_tertiaryContainer,
-//    onTertiaryContainer = md_theme_light_onTertiaryContainer,
     error = md_theme_light_error,
-//    errorContainer = md_theme_light_errorContainer,
     onError = md_theme_light_onError,
-//    onErrorContainer = md_theme_light_onErrorContainer,
     background = md_theme_light_background,
     onBackground = md_theme_light_onBackground,
     surface = md_theme_light_surface,
     onSurface = md_theme_light_onSurface,
-//    surfaceVariant = md_theme_light_surfaceVariant,
-//    onSurfaceVariant = md_theme_light_onSurfaceVariant,
-//    outline = md_theme_light_outline,
-//    inverseOnSurface = md_theme_light_inverseOnSurface,
-//    inverseSurface = md_theme_light_inverseSurface,
-//    inversePrimary = md_theme_light_inversePrimary,
-//    surfaceTint = md_theme_light_surfaceTint,
-//    outlineVariant = md_theme_light_outlineVariant,
-//    scrim = md_theme_light_scrim,
 )
 
 private val DarkColorScheme = darkColors(
     primary = md_theme_dark_primary,
     onPrimary = md_theme_dark_onPrimary,
-//    primaryContainer = md_theme_dark_primaryContainer,
-//    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
     secondary = md_theme_dark_secondary,
     onSecondary = md_theme_dark_onSecondary,
-//    secondaryContainer = md_theme_dark_secondaryContainer,
-//    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
-//    tertiary = md_theme_dark_tertiary,
-//    onTertiary = md_theme_dark_onTertiary,
-//    tertiaryContainer = md_theme_dark_tertiaryContainer,
-//    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
     error = md_theme_dark_error,
-//    errorContainer = md_theme_dark_errorContainer,
     onError = md_theme_dark_onError,
-//    onErrorContainer = md_theme_dark_onErrorContainer,
     background = md_theme_dark_background,
     onBackground = md_theme_dark_onBackground,
     surface = md_theme_dark_surface,
     onSurface = md_theme_dark_onSurface,
-//    surfaceVariant = md_theme_dark_surfaceVariant,
-//    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
-//    outline = md_theme_dark_outline,
-//    inverseOnSurface = md_theme_dark_inverseOnSurface,
-//    inverseSurface = md_theme_dark_inverseSurface,
-//    inversePrimary = md_theme_dark_inversePrimary,
-//    surfaceTint = md_theme_dark_surfaceTint,
-//    outlineVariant = md_theme_dark_outlineVariant,
-//    scrim = md_theme_dark_scrim,
 )
 
 private val AppShapes = Shapes(
@@ -98,72 +68,84 @@ private val AppShapes = Shapes(
     large = RoundedCornerShape(16.dp),
 )
 
+
 @Composable
-fun robotoThin() = FontFamily(
-    font(
-        "Roboto", "roboto_thin", FontWeight.Normal, FontStyle.Normal
-    )
+fun thinTypo() = FontFamily(
+    when (DEFAULT_TYPOGRAPHY) {
+        TypographyEmbedded.MONTSERRAT -> font(
+            "Montserrat",
+            "montserrat_thin",
+            Thin,
+            FontStyle.Normal
+        )
+
+        else -> {
+
+            font(
+                "Roboto", "roboto_thin", Thin, FontStyle.Normal
+            )
+        }
+    }
 )
 
 @Composable
-fun robotoLight() = FontFamily(
-    font(
-        "Roboto", "roboto_light", FontWeight.Normal, FontStyle.Normal
-    )
+fun lightTypo() = FontFamily(
+    when (DEFAULT_TYPOGRAPHY) {
+        TypographyEmbedded.MONTSERRAT -> font(
+            "Montserrat",
+            "montserrat_light",
+            Light,
+            FontStyle.Normal
+        )
+
+        else -> {
+
+            font(
+                "Roboto", "roboto_light", Light, FontStyle.Normal
+            )
+        }
+    }
 )
 
 @Composable
-fun robotoRegular() = FontFamily(
-    font(
-        "Roboto", "roboto", FontWeight.Normal, FontStyle.Normal
-    )
+fun regularTypo() = FontFamily(
+    when (DEFAULT_TYPOGRAPHY) {
+        TypographyEmbedded.MONTSERRAT -> font(
+            "Montserrat",
+            "montserrat_regular",
+            Normal,
+            FontStyle.Normal
+        )
+
+        else -> {
+
+            font(
+                "Roboto", "roboto", Normal, FontStyle.Normal
+            )
+        }
+    }
 )
 
 @Composable
-fun robotoBold() =
+fun boldTypo() =
     FontFamily(
-    font(
-        "Roboto", "roboto_bold", FontWeight.Normal, FontStyle.Normal
+        when (DEFAULT_TYPOGRAPHY) {
+            TypographyEmbedded.MONTSERRAT -> font(
+                "Montserrat",
+                "montserrat_bold",
+                Bold,
+                FontStyle.Normal
+            )
+
+            else -> {
+
+                font(
+                    "Roboto", "roboto_bold", Bold, FontStyle.Normal
+                )
+            }
+        }
     )
-)
 
-
-val typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    ),
-    body2 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
-    ),
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp,
-    ),
-    subtitle1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        color = Color.Gray
-    ),
-    subtitle2 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp,
-        color = Color.Gray
-    ),
-)
-
-private val AppTypography = typography
 
 internal val LocalThemeIsDark = compositionLocalOf { mutableStateOf(true) }
 
@@ -175,6 +157,41 @@ internal fun AppTheme(
     val systemIsDark = isSystemInDarkTheme()
     val isDarkState = remember { mutableStateOf(systemIsDark) }
 
+    val defaultTypography = Typography(
+        defaultFontFamily = regularTypo(),
+        h1 = TextStyle(
+            fontFamily = regularTypo(),
+            fontWeight = Bold,
+            fontSize = 30.sp,
+            color = Color.Black
+        ),
+        h2 = TextStyle(
+            fontFamily = regularTypo(),
+            fontWeight = SemiBold,
+            fontSize = 24.sp,
+            color = Color.Black
+        ),
+        h3 = TextStyle(
+            fontFamily = regularTypo(),
+            fontWeight = Medium,
+            fontSize = 20.sp,
+            color = Color.Black
+        ),
+        h4 = MaterialTheme.typography.h4.copy(fontFamily = regularTypo()),
+        h5 = MaterialTheme.typography.h5.copy(fontFamily = regularTypo()),
+        h6 = MaterialTheme.typography.h6.copy(fontFamily = regularTypo()),
+        body1 = MaterialTheme.typography.body1.copy(fontFamily = regularTypo()),
+        body2 = MaterialTheme.typography.body2.copy(fontFamily = regularTypo()),
+        caption = TextStyle(
+            fontFamily = regularTypo(),
+            fontWeight = Normal,
+            fontSize = 12.sp,
+        ),
+        subtitle1 = MaterialTheme.typography.subtitle1.copy(fontFamily = regularTypo()),
+        subtitle2 = MaterialTheme.typography.subtitle2.copy(fontFamily = regularTypo())
+    )
+
+
     CompositionLocalProvider(
         LocalThemeIsDark provides isDarkState
     ) {
@@ -183,9 +200,9 @@ internal fun AppTheme(
         SystemAppearance(!isDark)
 
         MaterialTheme(
-            typography = AppTypography,
+            typography = defaultTypography,
             shapes = AppShapes,
-            colors =  if (isDark) DarkColorScheme else LightColorScheme,
+            colors = if (isDark) DarkColorScheme else LightColorScheme,
             content = {
                 Surface(content = content)
             }
